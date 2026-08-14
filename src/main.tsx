1b39965edbe3a9c9fea8fb41113b833abcbe4ff4
Pie-ye <149685044+Pie-ye@users.noreply.github.com>
2026-08-14 15:31:16 +0800

    feat(graph): 掛載 GitKraken 式圖表互動

--- src/main.tsx
+++ src/main.tsx
@@ -1,14 +1,17 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { App } from "./App";
+import { GitKrakenGraphInteractions } from "./components/GitKrakenGraphInteractions";
 import { TrellisCanvasPortal } from "./components/trellis/TrellisCanvasPortal";
 import "./styles/app.css";
 import "./styles/liquid-glass.css";
+import "./styles/gitkraken.css";
 import "./styles/trellis.css";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <App />
+    <GitKrakenGraphInteractions />
     <TrellisCanvasPortal />
   </React.StrictMode>
 );
