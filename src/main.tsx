2c0f99cdaf67e87b663735f929b4f7272f6b6ae6
Pie-ye <149685044+Pie-ye@users.noreply.github.com>
2026-08-14 16:16:26 +0800

    feat(graph): mount GitKraken context actions

--- src/main.tsx
+++ src/main.tsx
@@ -1,16 +1,19 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { App } from "./App";
+import { GitKrakenContextActions } from "./components/GitKrakenContextActions";
 import { GitKrakenGraphInteractions } from "./components/GitKrakenGraphInteractions";
 import { TrellisCanvasPortal } from "./components/trellis/TrellisCanvasPortal";
 import "./styles/app.css";
 import "./styles/liquid-glass.css";
 import "./styles/gitkraken.css";
+import "./styles/gitkraken-context.css";
 import "./styles/trellis.css";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <App />
+    <GitKrakenContextActions />
     <GitKrakenGraphInteractions />
     <TrellisCanvasPortal />
   </React.StrictMode>
